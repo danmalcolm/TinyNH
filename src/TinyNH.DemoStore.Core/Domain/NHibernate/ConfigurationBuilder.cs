b652f245f7a802fd7e76fcfe7a1725b14419ecfb
Dan Malcolm <danmalc@gmail.com>
2013-04-15 00:10:13 +0100

    indenting

--- src/TinyNH.DemoStore.Core/Domain/NHibernate/ConfigurationBuilder.cs
+++ src/TinyNH.DemoStore.Core/Domain/NHibernate/ConfigurationBuilder.cs
@@ -11,40 +11,40 @@ namespace TinyNH.DemoStore.Core.Domain.NHibernate
     /// <summary>
     /// Creates NHibernate Configuration for the application's object model
     /// </summary>
-	public class ConfigurationBuilder
+    public class ConfigurationBuilder
     {
         private readonly Action<Configuration> customize;
-        
+
         /// <param name="customize">An optional action to customize the configuration after it has been built with standard settings. Intended to allow settings to be added when running tests, such as enabling statistics or logging sql</param>
         public ConfigurationBuilder(Action<Configuration> customize = null)
-		{
-		    this.customize = customize;
-		}
-		
-		public Configuration Build()
-		{
-			var configuration = new Configuration();
-            configuration.DataBaseIntegration(db =>
-			{
-				db.Dialect<MsSql2008Dialect>();
-				db.ConnectionStringName = "core";
-				db.BatchSize = 100;
-			});
-			configuration.CurrentSessionContext<CallSessionContext>();
+        {
+            this.customize = customize;
+        }
 
-			var props = new Dictionary<string, string>
+        public Configuration Build()
+        {
+            var configuration = new Configuration();
+            configuration.DataBaseIntegration(db =>
+            {
+                db.Dialect<MsSql2008Dialect>();
+                db.ConnectionStringName = "core";
+                db.BatchSize = 100;
+            });
+            configuration.CurrentSessionContext<CallSessionContext>();
+
+            var props = new Dictionary<string, string>
 			{
 				{"generate_statistics", "false"},
 				{"hbm2ddl.keywords", "auto-quote"}
 			};
-			configuration.AddProperties(props);
-			
-			ModelMapping.Add(configuration);
+            configuration.AddProperties(props);
+
+            ModelMapping.Add(configuration);
 
             if (customize != null)
                 customize(configuration);
 
-			return configuration;
-		}
-	}
+            return configuration;
+        }
+    }
 }
