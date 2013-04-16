3dfb62138dda1ae37050fd8015b5c50fc2eb7a94
Dan Malcolm <danmalc@gmail.com>
2013-04-16 22:41:52 +0100

    indent

--- src/TinyNH.DemoStore.Admin/Global.asax.cs
+++ src/TinyNH.DemoStore.Admin/Global.asax.cs
@@ -7,41 +7,41 @@ using NHibernate.Cfg;
 
 namespace TinyNH.DemoStore.Admin
 {
-	// Note: For instructions on enabling IIS6 or IIS7 classic mode, 
-	// visit http://go.microsoft.com/?LinkId=9394801
-	public class MvcApplication : System.Web.HttpApplication
-	{
-		protected void Application_Start()
-		{
-			AreaRegistration.RegisterAllAreas();
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
 
-			WebApiConfig.Register(GlobalConfiguration.Configuration);
-			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-			RouteConfig.RegisterRoutes(RouteTable.Routes);
-			SetupNHibernate();
-		}
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            SetupNHibernate();
+        }
 
-		private void SetupNHibernate()
-		{
-			var builder = new ConfigurationBuilder(c =>
+        private void SetupNHibernate()
+        {
+            var builder = new ConfigurationBuilder(c =>
                 c.CurrentSessionContext<LazyWebSessionContext>());
-			ConfigurationStore = new ConfigurationStore(builder.Build);
-		    LazyWebSessionContextModule.ConfigurationStore = ConfigurationStore;
-		}
+            ConfigurationStore = new ConfigurationStore(builder.Build);
+            LazyWebSessionContextModule.ConfigurationStore = ConfigurationStore;
+        }
 
-		public static ConfigurationStore ConfigurationStore { get; private set; }
+        public static ConfigurationStore ConfigurationStore { get; private set; }
 
-		/// <summary>
-		/// Gets the current NHibernate session made available for the current web request
-		/// </summary>
-		/// <remarks>
-		/// In this demo, we're choosing to make the current session available via a 
-		/// global property - there are other options that might scale better in a 
-		/// larger application, e.g. an IOC container.
-		/// </remarks>
-		public static ISession CurrentSession
-		{
-			get { return ConfigurationStore.SessionFactory.GetCurrentSession(); }
-		}
-	}
+        /// <summary>
+        /// Gets the current NHibernate session made available for the current web request
+        /// </summary>
+        /// <remarks>
+        /// In this demo, we're choosing to make the current session available via a 
+        /// global property - there are other options that might scale better in a 
+        /// larger application, e.g. an IOC container.
+        /// </remarks>
+        public static ISession CurrentSession
+        {
+            get { return ConfigurationStore.SessionFactory.GetCurrentSession(); }
+        }
+    }
 }
